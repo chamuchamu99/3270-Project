b6a991abb63a1fd02586d1fe308655f735f2cdf7
Colin <Colin@Colin-PC>
2016-12-01 15:10:35 -0500

    new class for basic flight object (implements serializable)

---
+++ 3270Project/src/edu/gsu/cis/project/FlightDetails.java
@@ -0,0 +1,53 @@
+package edu.gsu.cis.project;
+import java.io.*;
+public class FlightDetails implements Serializable {
+	public String Flight_Num;
+	public String FlightDate;
+	public String Time;
+	public int Num_Of_Seats;
+	public String Source;
+	public String Destination;
+
+    public FlightDetails(){
+    	super();
+	}
+
+//constructor for the flightDetails class
+    public FlightDetails(String fn,String fd, String t, int ns,String s,String d) {
+	    this.Flight_Num = fn;
+	    this.FlightDate = fd;
+	    this.Time = t;
+	    this.Num_Of_Seats = ns;
+	    this.Source = s;
+	    this.Destination =d;
+    }
+
+    public void setFlightNum(String flightNum) {
+	this.Flight_Num = flightNum;
+    }
+
+    public void setFlightDate(String FDate) {	
+    	this.FlightDate = FDate;
+    }
+
+    public void setTime(String time) {
+    	this.Time = time;
+    }
+
+    public void setNumSeats(int seats) {
+    	this.Num_Of_Seats = seats;
+    }
+
+    public void setSource(String SSource) {
+    	this.Source = SSource;
+    }
+
+    public void setDestination(String Des) {
+    	this.Destination = Des;
+    }
+
+    public void booked(int n) {
+    	this.Num_Of_Seats = this.Num_Of_Seats - n;
+    }
+
+}
--- 3270Project/src/edu/gsu/cis/project/HelloProject.java
+++ 3270Project/src/edu/gsu/cis/project/HelloProject.java
@@ -2,7 +2,7 @@ package edu.gsu.cis.project;
 
 public class HelloProject {
     public static void main(String[] args) {
-    	System.out.println("hello April");
+    	System.out.println("hello");
     	System.out.println("test 1");
     }
 }
